8a72f472c4477f61539782d9fb4062b994246f86
minhdeii66@gmail.com <minhdeii66@gmail.com>
2023-03-09 15:00:45 +0700

    working on update category

--- src/pages/Admin/Topic/TopicList.tsx
+++ src/pages/Admin/Topic/TopicList.tsx
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Flex, Icon, Link, LinkOverlay, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
 import DeleteConfirmationModal from "@/components/Modal/DeleteConfirmation";
 import router from "next/router";
 import { AiFillDelete, AiFillEdit, AiFillInfoCircle } from "react-icons/ai";
 import {IoMdArrowRoundBack } from "react-icons/io";
+import { GetServerSidePropsContext } from "next";
+import axios from "axios";
+import safeJsonStringify from "safe-json-stringify";
+import { Topic } from "@/atoms/topicAtom";
 
-const submit: React.FC = () => {
+type topicProps = {
+  TopicData: Topic[];
+}
+const TopicList: React.FC<topicProps> = ({TopicData}) => {
+  const [topicList, setTopicList] = useState<Topic[]>([]);
+useEffect(() => {
+    setTopicList(TopicData);
+  }, []);
   const [displayConfirmationModal, setDisplayConfirmationModal] = useState(false);
   const showDeleteModal = () => {
     setDisplayConfirmationModal(true);
@@ -29,6 +40,7 @@ const submit: React.FC = () => {
     <Thead>
       <Tr>
         <Th fontSize={16}>Topic Name</Th>
+        <Th fontSize={16}>Modify Date</Th>
         <Th fontSize={16}> CLosure Date</Th>
         <Th fontSize={16}>Final Closure Date</Th>
         <Th fontSize={16}>Status</Th>
@@ -36,30 +48,34 @@ const submit: React.FC = () => {
       </Tr>
     </Thead>
     <Tbody>
-      <Tr>
-        <Td  fontWeight={900} >First topic</Td>
-        <Td >27/2/2023</Td>
-        <Td >28/2/2023</Td>
-        <Td >Open</Td>
-        <Td justifyItems="center">
-        <Icon as={AiFillInfoCircle} 
-        fontSize={40} color="white" 
-         _hover={{ color: "yellow" }}
-         onClick={() => router.push('/Admin' +'/Topic' + '/TopicDetails')} />
-        
-        <Icon as={AiFillEdit} 
-        fontSize={40} color="white" 
-         _hover={{ color: "blue.300" }}
-         ml="20px"
-         onClick={() => router.push('/Admin' +'/Topic' + '/TopicEdit')}/>
+      {topicList.map((item) => (
+                <Tr>
+                <Td  fontWeight={900} >{item.name}</Td>
+                <Td >{item.modifyDate}</Td>
+                <Td >{item.idea_closure_date}</Td>
+                <Td >{item.final_closure_date}</Td>
+                <Td >Open</Td>
+                <Td justifyItems="center">
+                <Icon as={AiFillInfoCircle} 
+                fontSize={40} color="white" 
+                 _hover={{ color: "yellow" }}
+                 onClick={() => router.push('/Admin' +'/Topic' + '/TopicDetails')} />
+                
+                <Icon as={AiFillEdit} 
+                fontSize={40} color="white" 
+                 _hover={{ color: "blue.300" }}
+                 ml="20px"
+                 onClick={() => router.push('/Admin' +'/Topic' + '/TopicEdit')}/>
+              
+                <Icon as={AiFillDelete} 
+                fontSize={40} color="white" 
+                 _hover={{ color: "red" }}
+                 ml="20px"
+                 onClick={() => showDeleteModal()} />
+                </Td>
+              </Tr>
+              ))}
       
-        <Icon as={AiFillDelete} 
-        fontSize={40} color="white" 
-         _hover={{ color: "red" }}
-         ml="20px"
-         onClick={() => showDeleteModal()} />
-        </Td>
-      </Tr>
      
     </Tbody>
   </Table>
@@ -68,5 +84,22 @@ const submit: React.FC = () => {
         <DeleteConfirmationModal showModal={displayConfirmationModal} confirmModal={submitDelete} hideModal={hideConfirmationModal}   />
       </>
   );
+  
 };
-export default submit;
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  //get topic data and pass it to cline
+  //context.query.topicId as string => getting id from route
+  try {
+    const response = await axios.get('http://localhost:8080/topic');
+    console.log(response.data);
+    return {
+      props: {
+        TopicData: JSON.parse(safeJsonStringify([...response.data])),
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+export default TopicList;
