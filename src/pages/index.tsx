f5e7c0c34303e8f70e54b22240f8cf562cce86b7
Mins <minhdeii66@gmail.com>
2023-03-15 23:58:44 +0700

    finish on basic search

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { Heading, Stack } from "@chakra-ui/react";
+import { useEffect, useState } from 'react';
+import { Heading, Stack } from '@chakra-ui/react';
 import {
   collection,
   DocumentData,
@@ -10,22 +10,22 @@ import {
   query,
   QuerySnapshot,
   where,
-} from "firebase/firestore";
-import type { NextPage } from "next";
-import { useRouter } from "next/router";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useRecoilState, useRecoilValue } from "recoil";
+} from 'firebase/firestore';
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useRecoilState, useRecoilValue } from 'recoil';
 // import { communityState } from "../atoms/communitiesAtom";
 // import { Post, PostVote } from "../atoms/postsAtom";
 // import CreatePostLink from "../components/Community/CreatePostLink";
 // import Recommendations from "../components/Community/Recommendations";
-import PageContentLayout from "../components/Layout/PageContent";
-import { auth, firestore } from "@/Firebase/clientApp";
-import Recommendations from "@/components/Topic/Recommendations";
-import axios from "axios";
-import { Idea, ideaState } from "@/atoms/ideaAtom";
-import IdeaItem from "@/components/Posts/IdeaItem";
-import FollowTopics from "@/components/Topic/FollowedTopics";
+import PageContentLayout from '../components/Layout/PageContent';
+import { auth, firestore } from '@/Firebase/clientApp';
+import Recommendations from '@/components/Topic/Recommendations';
+import axios from 'axios';
+import { Idea, ideaState } from '@/atoms/ideaAtom';
+import IdeaItem from '@/components/Posts/IdeaItem';
+import FollowTopics from '@/components/Topic/FollowedTopics';
 // import PostLoader from "../components/Post/Loader";
 // import PostItem from "../components/Post/PostItem";
 // import { auth, firestore } from "../firebase/clientApp";
@@ -35,7 +35,7 @@ import FollowTopics from "@/components/Topic/FollowedTopics";
 
 const Home: NextPage = () => {
   const [user, loadingUser] = useAuthState(auth);
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   const [ideaStateValue, setIdeaStateValue] = useRecoilState(ideaState);
   // const {
   //   postStateValue,
@@ -49,35 +49,33 @@ const Home: NextPage = () => {
   // const communityStateValue = useRecoilValue(communityState);
 
   const getTrendingPosts = async () => {
-    setLoading(true)
-    console.log("Trending Post");
+    setLoading(true);
+    console.log('Trending Post');
     try {
-      axios.get('http://localhost:8080/idea').then(res => {
-        console.log("Get trending ideas", res);
+      axios.get('http://localhost:8080/idea').then((res) => {
+        console.log('Get trending ideas', res);
         setIdeaStateValue((prev) => ({
           ...prev,
-          Ideas: res.data
+          Ideas: res.data,
         }));
-      })
+      });
     } catch (error) {
-      console.log("Get trending ideas error", error);
+      console.log('Get trending ideas error', error);
     }
     setLoading(false);
   };
 
   const getNoUserHomePosts = async () => {
-    console.log("GETTING NO USER FEED");
+    console.log('GETTING NO USER FEED');
     //setLoading(true);
     //setLoading(false);
   };
 
-  const getUserPostVotes = async () => {
-
-  };
+  const getUserPostVotes = async () => {};
 
   useEffect(() => {
     getTrendingPosts();
-  }, [])
+  }, []);
 
   // useEffect(() => {
   //   /**
@@ -116,14 +114,13 @@ const Home: NextPage = () => {
       <>
         <Stack>
           <Heading
-            bgGradient='linear(to-l, brand.100, brand.200)'
-            bgClip='text'
-          >Trending Ideas</Heading>
+            bgGradient="linear(to-l, brand.100, brand.200)"
+            bgClip="text"
+          >
+            Trending Ideas
+          </Heading>
           {ideaStateValue.Ideas.map((idea: Idea, index) => (
-            <IdeaItem
-              idea={idea}
-              index={index}
-            />
+            <IdeaItem idea={idea} index={index} />
           ))}
         </Stack>
       </>
